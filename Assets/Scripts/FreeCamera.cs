1cbce1ceff3eda2289f5936687b407a949615d50
Unknown <Appietro516@email.arizona.edu>
2018-10-14 11:52:04 -0700

    Created Free Camera/added plugin for bezier curves

---
+++ Assets/Scripts/FreeCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeCamera : MonoBehaviour {
+
+	private Vector3 initpos;
+
+	void Start () {
+		initpos = this.gameObject.transform.position;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		float scroll = Input.GetAxis("Scroll");
+
+		if (horizontal != 0){
+			this.gameObject.transform.position += new Vector3(horizontal,0,0);
+		}
+
+
+		if (vertical != 0){
+			this.gameObject.transform.position += new Vector3(0,0,vertical);
+		}
+
+		if (scroll != 0){
+			this.gameObject.transform.position += new Vector3(0,-1*scroll,0);
+		}
+
+		if (Input.GetButtonDown("Reset")){
+			this.gameObject.transform.position = initpos;
+		}
+
+	}
+}
---
+++ Assets/Scripts/SplineTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DentedPixel;
+
+using UnityEngine.SceneManagement;
+
+public class SplineTest : MonoBehaviour {
+	public GameObject lt;
+	public int speed;
+
+	void Awake () {
+		SceneManager.sceneLoaded += this.callback;
+	}
+
+	void callback(Scene scene, LoadSceneMode sceneMode){
+		print("got callback");
+		LTBezierPath ltPath = new LTBezierPath( new Vector3[] { new Vector3(0f,.5f,0f),new Vector3(-5f,.5f,8f), new Vector3(-5f,.5f,2f), new Vector3(0f,.5f,10f)} );
+
+		LeanTween.move(lt, ltPath, ltPath.length/speed); // animate
+
+	}
+}

8e4d634fde45734c3cf3179eafe0529c2f01f4c7
Unknown <Appietro516@email.arizona.edu>
2018-10-23 20:39:21 -0700

    modified elevation behavior/minor adjustments to path generator

--- Assets/Scripts/CreatePath.cs
+++ Assets/Scripts/CreatePath.cs
@@ -16,6 +16,8 @@ public class CreatePath : MonoBehaviour {
 	public LTSpline ltpath;
 	public int segments;
 
+	public float path_y_offset = .5f;
+
 	private Vector3 start = Vector3.zero;
 	private Vector3 end = Vector3.zero;
 
@@ -42,7 +44,8 @@ public class CreatePath : MonoBehaviour {
 		}
 
 		ltpath = generate_spline();
-		Instantiate(core, new Vector3(end.x, core.GetComponent<MeshFilter>().sharedMesh.bounds.extents.y/2,end.z), Quaternion.identity);
+		Instantiate(core, new Vector3(end.x, end.y, end.z), Quaternion.identity);
+		//+ core.GetComponent<MeshFilter>().sharedMesh.bounds.extents.y/2
 	}
 
 	// Update is called once per frame
@@ -51,7 +54,7 @@ public class CreatePath : MonoBehaviour {
 	}
 
 	private LTBezierPath generate_bezier(){
-		start = new Vector3(Random.Range(-1*map_size/2, map_size/2), 0,-1*map_size/2);
+		start = new Vector3(Random.Range(-1*map_size/2, map_size/2), path_y_offset,-1*map_size/2);
 
 		Vector3[] current_path = new Vector3[4+(segments*4)];
 		current_path[0] = start;
@@ -78,10 +81,10 @@ public class CreatePath : MonoBehaviour {
 
 		}
 
-		end =  new Vector3(Random.Range(-1*map_size/2 , map_size/2), 0,map_size/2);
+		end =  new Vector3(Random.Range(-1*map_size/2 , map_size/2), path_y_offset,map_size/2);
 		current_path[(segments*4)+1] = generate_random_z(inter.z, map_size/2);
 		current_path[(segments*4)+1].x = end.x;
-		current_path[(segments*4)+2] = new Vector3(end.x, 0, end.z-1);
+		current_path[(segments*4)+2] = new Vector3(end.x, path_y_offset, end.z-1);
 		current_path[(segments*4)+3] = end;
 
 		return new LTBezierPath(current_path);
@@ -91,26 +94,26 @@ public class CreatePath : MonoBehaviour {
 
 	private LTSpline generate_spline(){
 		Vector3[] current_path = new Vector3[4+(segments)];
-		Vector3 start_control = new Vector3(0,0,0);
+		Vector3 start_control = new Vector3(0,path_y_offset,0);
 
 		current_path[0] = start_control;
 
-		Vector3 start = new Vector3(Random.Range(-1*map_size/2, map_size/2), 0,-1*map_size/2);
+		Vector3 start = new Vector3(Random.Range(-1*map_size/2, map_size/2), path_y_offset,-1*map_size/2);
 		current_path[1] = start;
 
 		Vector3 inter = start;
-		Vector3 last_inter = new Vector3(map_size,0, map_size); //impossible vector
+		Vector3 last_inter = new Vector3(map_size, path_y_offset, map_size); //impossible vector
 		for(int i = 0; i < segments; i++){
 
-			inter = generate_random_z(inter.z, inter.z + (map_size/2 - inter.z)/( segments-i));
+			inter = generate_random_z(inter.z + 1, inter.z + (map_size/2 - inter.z)/( segments-i));
 
 
 			current_path[i + 2]  = inter;
 			last_inter = inter;
 		}
 
-		end =  new Vector3(Random.Range(inter.x , map_size/2), 0,map_size/2);
-		Vector3 end_control = new Vector3(end.x,0,end.z-1);
+		end =  new Vector3(Random.Range(inter.x, map_size/2), path_y_offset,map_size/2);
+		Vector3 end_control = new Vector3(end.x, path_y_offset,end.z-1);
 		current_path[3+(segments)] = end_control;
 		current_path[2+(segments)] = end;
 
@@ -120,16 +123,16 @@ public class CreatePath : MonoBehaviour {
 	}
 
 	private Vector3 generate_random(){
-		return new Vector3(Random.Range(-1*map_size/2, map_size/2), 0,Random.Range(-1*map_size/2, map_size/2));
+		return new Vector3(Random.Range(-1*map_size/2, map_size/2), path_y_offset,Random.Range(-1*map_size/2, map_size/2));
 	}
 
 
 	private Vector3 generate_random_z(float z_constraint,float z_constraint2){
-		return new Vector3(Random.Range(-map_size/2, map_size/2), 0,Random.Range(z_constraint, z_constraint2));
+		return new Vector3(Random.Range(-map_size/2, map_size/2), path_y_offset,Random.Range(z_constraint, z_constraint2));
 	}
 
 	private Vector3 generate_random_x(float x_constraint1,float x_constraint2){
-		return new Vector3(Random.Range(x_constraint1, x_constraint2), 0,Random.Range(-map_size/2, map_size/2));
+		return new Vector3(Random.Range(x_constraint1, x_constraint2), path_y_offset,Random.Range(-map_size/2, map_size/2));
 	}
 
 

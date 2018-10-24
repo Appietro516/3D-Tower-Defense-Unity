188209e38dd7525a1b748535fb2d2bb723787947
Unknown <Appietro516@email.arizona.edu>
2018-10-23 21:40:07 -0700

    redid healthbar to accept dynamic amounts of health

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,14 +7,16 @@ public class HealthBar : MonoBehaviour {
 	public RectTransform healthBar;
 	public Enemy enemy;
 
+	private float scale;
 
 
 	// Use this for initialization
 	void Start () {
+		scale = healthBar.sizeDelta.x/enemy.health;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		healthBar.sizeDelta = new Vector2(enemy.health, healthBar.sizeDelta.y);
+		healthBar.sizeDelta = new Vector2(enemy.health*scale, healthBar.sizeDelta.y);
 	}
 }

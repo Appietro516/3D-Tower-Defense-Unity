5954f454d083235629662d87bb43bc57d05bddf6
Anthony <Appietro516@email.arizona.edu>
2018-10-29 21:05:32 -0700

    bug fixes

--- Assets/Scripts/Core.cs
+++ Assets/Scripts/Core.cs
@@ -3,15 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Core : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
-
-	}
-
-	// Update is called once per frame
 	void Update () {
 		if(this.isDead()){
-			print("GAME OVER");
+			PlayerBehaviors.gamover = true;
 		}
 
 	}
--- Assets/Scripts/PlayerBehaviors.cs
+++ Assets/Scripts/PlayerBehaviors.cs
@@ -5,28 +5,29 @@ using UnityEngine;
 public class PlayerBehaviors : MonoBehaviour {
 
 	[SerializeField] public static int money = 100;
-	[SerializeField] public static int health = 100;
+	[SerializeField] public static int health = 50;
 	[SerializeField] public static int wave = 0;
 
 	public static bool paused = false;
+	public static bool gamover = false;
 
 	public GameObject pausetext;
 	public GameObject controls;
 	public GameObject reminder;
+	public GameObject exits;
 
-	// Use this for initialization
-	void Start () {
 
-	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Pause")){
-		   Time.timeScale = -1*Time.timeScale + 1;
-		   paused = !paused;
-		   pausetext.SetActive(!pausetext.activeSelf);
+		if (!gamover){
+			if (Input.GetButtonDown("Pause")){
+			   Time.timeScale = -1*Time.timeScale + 1;
+			   paused = !paused;
+			   pausetext.SetActive(!pausetext.activeSelf);
 
-	   	}
+		   	}
+		}
 
 		if (Input.GetButtonDown("Controls")){
 		 	controls.SetActive(!controls.activeSelf);
@@ -36,6 +37,11 @@ public class PlayerBehaviors : MonoBehaviour {
 
 		if (Input.GetButtonDown("Exit")){
 			 Application.Quit();
+		}
+
+		if(gamover){
+			Time.timeScale = 0;
+			exits.SetActive(true);
 		}
 
 	}

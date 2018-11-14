e14dcf64a05092774c28048880fff5d7c7a58518
Anthony <Appietro516@email.arizona.edu>
2018-11-13 22:15:59 -0700

    wrote code for slow tower

---
+++ Assets/Scripts/Towers/SlowTower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowTower : MonoBehaviour {
+
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public class AoeTower : AbstractTower {
+		public override void Start () {
+			base.Start();
+		}
+
+		protected override bool fire(){
+			foreach(GameObject targetedEnemy in CreatePath.enemies){
+				if (inRange(targetedEnemy)){
+					Enemy enemy_stats = targetedEnemy.GetComponent<Enemy>();
+					enemy_stats.speed-= damage;
+				}
+			}
+			since_fired = 0;
+			return true;
+		}
+
+		protected override void miscUpdate(){
+			//add ice explosion animation
+		}
+
+
+	}
+
+}

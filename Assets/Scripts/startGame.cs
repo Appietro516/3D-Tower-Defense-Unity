863008a12d08fa963f28b9b89f508ac75f0f08c3
Anthony <Appietro516@email.arizona.edu>
2018-11-24 21:18:55 -0700

    Added basic menu screen

---
+++ Assets/Scripts/startGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+public class startGame : MonoBehaviour {
+
+
+	public Button start;
+	void Start () {
+        start.onClick.AddListener(Load_First_Level);
+
+	}
+
+
+    public void Load_First_Level(){
+        print("Loading");
+        SceneManager.LoadScene("basic_level", LoadSceneMode.Single);
+    }
+}

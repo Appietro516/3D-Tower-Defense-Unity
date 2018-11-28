d8ee0e42748128c1995f6c3aec4337e997406769
Anthony <Appietro516@email.arizona.edu>
2018-11-27 23:11:20 -0700

    code for bomb tower

---
+++ Assets/Scripts/Towers/Bomb.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//the bank tower does not need to update itself, so most  methods are empty.
+public class Bomb : AbstractTower {
+
+	public override void Start () {
+		base.Start();
+	}
+
+	protected override void miscUpdate(){
+		return;
+	}
+
+	protected override bool fire(){
+		return true;
+	}
+
+	void OnMouseDown(){
+		Destroy(this.gameObject);
+		foreach(GameObject targetedEnemy in CreatePath.enemies){
+			if (inRange(targetedEnemy)){
+				targetedEnemy.health = targetedEnemy.health/2;
+			}
+
+	}
+
+
+
+}
